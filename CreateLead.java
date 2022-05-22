cf10dd4a32e48bfd23d66b7b6cba0b703c6a9f8d
Ram-Prasad-Manivannan <105555632+Ram-Prasad-Manivannan@users.noreply.github.com>
2022-05-22 10:21:46 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,46 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead 
+{
+
+	public static void main(String[] args) 
+	{
+		  WebDriverManager.chromedriver().setup();
+		  ChromeDriver driver=new ChromeDriver();
+		  driver.get("http://leaftaps.com/opentaps/control/login");
+		  driver.manage().window().maximize();
+		  driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		  
+		  driver.findElement(By.id("password")).sendKeys("crmsfa");
+		  
+		  driver.findElement(By.className("decorativeSubmit")).click();
+		  driver.findElement(By.linkText("CRM/SFA")).click();
+		  driver.findElement(By.linkText("Leads")).click();
+		  driver.findElement(By.linkText("Create Lead")).click();
+		  driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Dawns");
+		  driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Ram ");
+		  driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Prasad M");
+		  driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Ram Prasad M");
+		  driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing department");
+		  driver.findElement(By.id("createLeadForm_description")).sendKeys("Life is so awesome in testing");
+		  
+		  driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("ramprasadmanivannan@gmail.com");
+		  
+		  WebElement drop=driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		  Select dd=new Select(drop);
+		  dd.selectByVisibleText("New York");
+		  
+		  driver.findElement(By.name("submitButton")).click();
+		  
+		  System.out.println("Title of Web Page is :"+driver.getTitle());
+		  
+	}
+
+}
